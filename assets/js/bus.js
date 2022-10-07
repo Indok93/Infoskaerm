afa6a5ea3570a892fe5444225cdf54f94b91df57
Tobias Aagaard <Tobias909@live.dk>
2022-10-07 11:55:04 +0200

    Bus api
    bus api

--- assets/js/bus.js
+++ assets/js/bus.js
@@ -1,29 +1,35 @@
-//apiData is the arry for the full products array.
-// it is the main array
-const apiData = [];
-
 const apiEndPoint =
   "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1";
+let apiData = [];
 
-const getApiData = () => {
+for (let i = 0; i < 7; i++) {
   fetch(apiEndPoint)
     .then((response) => {
+      //parsing data
       return response.json();
     })
     .then((data) => {
-      console.log(data);
-      apiData.push(...data.MultiDepartureBoard.Departure);
+      //The data you wanna use
+      apiData = data.MultiDepartureBoard.Departure[i];
+      console.log(apiData);
     })
     .catch((error) => {
+      //If theres an error
       console.error(error);
     })
     .finally(() => {
-      renderProducts();
+      renderBusLine();
     });
-};
+}
 
-const renderProducts = (array) => {
-  console.log(apiData);
-};
+let renderBusLine = () => {
+  let arrayData = apiData.def == undefined;
 
-getApiData();
+  if (arrayData) {
+    document.querySelector("#bus").innerHTML += `
+     <p class="">${apiData.direction}</p>
+     <p>${apiData.line}</p>
+    <p>${apiData.time}</p>
+ `;
+  }
+};
